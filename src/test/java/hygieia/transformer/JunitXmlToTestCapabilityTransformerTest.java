8cc11bbcad2eed9993bb3a948b48fdd55c17f6ca
Oliver D'Costa <oliver.d-costa@capgemini.com>
2021-01-03 02:30:25 -0600

    first commit

---
+++ src/test/java/hygieia/transformer/JunitXmlToTestCapabilityTransformerTest.java
@@ -0,0 +1,59 @@
+//
+//package hygieia.transformer;
+//
+//import static org.hamcrest.Matchers.equalTo;
+//import static org.hamcrest.Matchers.is;
+//import static org.junit.Assert.assertThat;
+//
+//import java.io.File;
+//import java.io.FileReader;
+//import java.net.URL;
+//
+//import javax.xml.bind.JAXBContext;
+//import javax.xml.bind.JAXBException;
+//import javax.xml.bind.Unmarshaller;
+//
+//import org.junit.Test;
+//
+//import com.capitalone.dashboard.model.TestCapability;
+//import com.capitalone.dashboard.model.TestSuite;
+//import com.capitalone.dashboard.model.quality.JunitXmlReport;
+//
+//public class JunitXmlToTestCapabilityTransformerTest {
+//
+//    JunitXmlToTestCapabilityTransformer sut = new JunitXmlToTestCapabilityTransformer();
+//
+//    private static  JAXBContext jaxbContext;
+//
+//    static {
+//        try {
+//            jaxbContext = JAXBContext.newInstance(JunitXmlReport.class);
+//        } catch (JAXBException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @Test
+//    public void producesJunitTestResult() throws Exception {
+//
+//        URL url = getClass().getResource("/junit.xml");
+//        File file = new File(url.getPath());
+//        FileReader fileReader = new FileReader(file);
+//        JunitXmlReport responseReport = null;
+//
+//        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+//        responseReport = (JunitXmlReport) unmarshaller.unmarshal(fileReader);
+//
+//
+//        TestCapability capability = sut.convert(responseReport);
+//
+//
+//        assertThat(capability.getSuccessTestSuiteCount(), is(equalTo(1)));
+//        assertThat(capability.getFailedTestSuiteCount(), is(equalTo(0)));
+//        assertThat(capability.getTestSuites().size(), is(equalTo(1)));
+//        TestSuite suite = capability.getTestSuites().iterator().next();
+//        assertThat(suite.getTestCases().size(), is(equalTo(4)));
+//    }
+//
+//}
+//
